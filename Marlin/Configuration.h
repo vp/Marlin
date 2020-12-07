9cd993d38ba4135882c89b305d329968880cb08e
Vaclav Prokes <vprokes@mouse-over.net>
2020-12-07 21:20:19 +0100

    PID tuning values

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -131,7 +131,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "E3ProSkr1.4 v0.4"
+#define CUSTOM_MACHINE_NAME "E3ProSkr1.4 v0.5"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
@@ -500,9 +500,9 @@
     #define DEFAULT_Ki_LIST {   1.54,   1.54 }
     #define DEFAULT_Kd_LIST {  76.55,  76.55 }
   #else
-    #define DEFAULT_Kp  21.73
-    #define DEFAULT_Ki   1.54
-    #define DEFAULT_Kd  76.55
+    #define DEFAULT_Kp  20.89
+    #define DEFAULT_Ki   1.43
+    #define DEFAULT_Kd  76.48
   #endif
 #endif // PIDTEMP
 
@@ -541,9 +541,9 @@
 
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  #define DEFAULT_bedKp 160.01
+  #define DEFAULT_bedKi 27.49
+  #define DEFAULT_bedKd 620.95
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
